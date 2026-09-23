33f4b1a0654248944bbdb2ef2e237a77f45ca918
vishnu.subhash <vishnu.subhash@benzyinfotech.com>
2026-09-23 17:54:54 +0530

    year fix

--- js/main.js
+++ js/main.js
@@ -9,8 +9,6 @@ document.addEventListener('DOMContentLoaded', () => {
     highlightActiveNavLink();
 });
 
-document.getElementById("current-year").textContent = new Date().getFullYear();
-
 // 1. Page Preloader
 function initPreloader() {
     const preloader = document.getElementById('page-preloader');
